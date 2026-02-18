6e4c3cf099e3054dc137be0a4b6d0f6319742e06
barrblga <barrblga@gmail.com>
2026-02-18 18:22:19 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -297,5 +297,4 @@ def add_ad():
     return render_template('add_ad.html', categories=categories)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(debug=True)
