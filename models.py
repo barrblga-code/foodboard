1ff743d1ad85d52f39610568a377fc0008730a6c
barrblga <barrblga@gmail.com>
2026-02-18 23:52:12 +0500

    Update models.py

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class Category(db.Model):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(300), nullable=False)  # ← было 150, стало 300
     name = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(50), nullable=False)
     city = db.Column(db.String(100), nullable=False)
@@ -27,7 +27,6 @@ class User(UserMixin, db.Model):
     ads = db.relationship('Ad', backref='seller', lazy=True)
     favorites = db.relationship('Ad', secondary=favorites, lazy='subquery',
                                 backref=db.backref('favorited_by', lazy=True))
-
 class Ad(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
@@ -37,4 +36,5 @@ class Ad(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
     category = db.relationship('Category', backref='ads')
+
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
